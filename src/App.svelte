e63e796bbb919357a7982ccf09d795d55b3a30bd
connerdouglass <conner.douglass@gmail.com>
2021-08-14 12:02:51 -0700

    Added coming soon text

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-	export let name: string;
+	// export let name: string;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>cdd</h1>
+	<p>Site coming soon</p>
 </main>
 
 <style>
