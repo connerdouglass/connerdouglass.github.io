3d62c0772c3cba6344d043007aa7e4f9851b3cd1
connerdouglass <conner.douglass@gmail.com>
2021-11-04 18:09:01 -0700

    Updated list of open source repos

--- src/data/open_source_repos.ts
+++ src/data/open_source_repos.ts
@@ -2,11 +2,46 @@ import type { IRepo } from "../components/repo";
 
 export const Repos: IRepo[] = [
     {
+        title: 'spireav',
+        url: 'https://github.com/spiretechnology/spireav',
+        description: 'Graph-based video transcoding library',
+        tags: [
+            'golang',
+            'ffmpeg',
+        ],
+    },
+    {
         title: 'go-timecode',
         url: 'https://github.com/spiretechnology/go-timecode',
         description: 'SMPTE timecode parsing and calculation library in Go',
         tags: [
             'golang',
+        ],
+    },
+    {
+        title: 'go-retry',
+        url: 'https://github.com/connerdouglass/go-retry',
+        description: 'Go library for automatic retries with exponential, randomized backoff',
+        tags: [
+            'golang',
+        ],
+    },
+    {
+        title: 'go-pool',
+        url: 'https://github.com/spiretechnology/go-pool',
+        description: 'Priority worker pools library in Go',
+        tags: [
+            'golang',
+            'workers',
+        ],
+    },
+    {
+        title: 'go-geo',
+        url: 'https://github.com/connerdouglass/go-geo',
+        description: 'Geolocation and distance calculation library in Go',
+        tags: [
+            'golang',
+            'geolocation',
         ],
     },
     {
